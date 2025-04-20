d26a02fe343213b9c39b9d455cee9a8253faed43
kidoneself <8768611@qq.com>
2025-04-20 17:22:44 +0800

    删除所有mock相关代码

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -10,10 +10,7 @@ import { VAxios } from './Axios';
 import type { AxiosTransform, CreateAxiosOptions } from './AxiosTransform';
 import { formatRequestDate, joinTimestamp, setObjToUrlParams } from './utils';
 
-const env = import.meta.env.MODE || 'development';
-
-// 如果是mock模式 或 没启用直连代理 就不配置host 会走本地Mock拦截 或 Vite 代理
-const host = env === 'mock' || import.meta.env.VITE_IS_REQUEST_PROXY !== 'true' ? '' : import.meta.env.VITE_API_URL;
+const host = import.meta.env.VITE_IS_REQUEST_PROXY !== 'true' ? '' : import.meta.env.VITE_API_URL;
 
 // 数据处理，方便区分多种处理方式
 const transform: AxiosTransform = {
@@ -203,4 +200,5 @@ function createAxios(opt?: Partial<CreateAxiosOptions>) {
     ),
   );
 }
+
 export const request = createAxios();
